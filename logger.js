e40a40befd33ffd4f4720259cafb002ca5211260
Faizan <sfaizan78692@gmail.com>
2025-01-13 07:57:45 +0530

    make text message colorful

--- logger.js
+++ logger.js
@@ -1,20 +1,16 @@
 import chalk from "chalk";
 
 export class Logger {
-  errorLog = chalk.white.bgRed.bold;
-  infoLog = chalk.white.bgBlue.bold;
-
   log(type, label, message, data) {
     const formattedData = data ? `\n${JSON.stringify(data, null, 2)}` : "";
+    const messageStyle = type === "error" ? chalk.redBright : chalk.blueBright;
+    const labelStyle =
+      type === "error" ? chalk.white.bgRed : chalk.white.bgBlue;
 
     if (process.env.NODE_ENV === "production") {
       console[type](label, message, formattedData);
     } else {
-      console[type](
-        type === "error" ? this.errorLog(label) : this.infoLog(label),
-        message,
-        formattedData
-      );
+      console[type](labelStyle(label), messageStyle(message), formattedData);
     }
   }
 
